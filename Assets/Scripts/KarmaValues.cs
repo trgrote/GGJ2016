15b138763d6f6cd5a2b776e7b38a270f52477377
Taylor Grote <grotese@gmail.com>
2016-01-29 19:56:43 -0600

    Add Karma Values globals and sublime project

---
+++ Assets/Scripts/KarmaValues.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class KarmaValues
+{
+	public static int Nature;
+	public static int Civilzation;
+	
+	public static int Good;
+	public static int Evil;
+
+	public static int Apathy;
+	public static int Active;
+}
