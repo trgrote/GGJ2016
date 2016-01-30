d59e32567c91a41e0a6155303bdb35e369bf392e
Forrest Catlett <eysohn92@outlook.com>
2016-01-30 14:38:11 -0600

    added prefabs

---
+++ Assets/Scripts/NextLevel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityStandardAssets._2D
+{
+	public class NextLevel : MonoBehaviour
+	{
+		private void OnTriggerEnter2D(Collider2D other)
+		{
+			if (other.tag == "Player")
+			{
+				SceneManager.LoadScene(0);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/ZoneChangeHandler.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoneChangeHandler : MonoBehaviour {
+
+	public Transform CameraCenter;
+	public Transform PeaceCenter;
+	public Transform DangerCenter;
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "PeaceZone") {
+			CameraCenter.position = PeaceCenter.position;
+			Debug.Log ("PeaceZone");
+		} else if (other.tag == "DangerZone") {
+			CameraCenter.position = DangerCenter.position;
+			Debug.Log ("DangerZone");
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
