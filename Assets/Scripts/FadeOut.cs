a5f21831632852186ed47ed91edddb202d7bf094
Taylor Grote <grotese@gmail.com>
2016-01-30 17:43:14 -0600

    Add Fadeout script

---
+++ Assets/Scripts/FadeOut.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class FadeOut : MonoBehaviour 
+{
+	[SerializeField] private float _speed = 2f;
+
+	void Start()
+	{
+		StartFadeOut();
+	}
+	void StartFadeOut()
+	{
+		SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+		StartCoroutine( FadeOutCoroutine( sprite ) );
+	}
+
+	IEnumerator FadeOutCoroutine(SpriteRenderer sprite)
+	{
+		Color blank_color = sprite.color;
+		blank_color.a = 0.0f;
+		while ( sprite.color.a > 0.05f )
+		{
+			sprite.color = Color.Lerp( sprite.color, blank_color, Time.deltaTime * _speed);
+			yield return null;
+		}
+		sprite.color = blank_color;
+		Debug.Log("Fade Out Complete");
+	}
+}
