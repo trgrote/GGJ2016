5661906a4458223d75941a8f8c52f4c891bf8cb1
Taylor Grote <grotese@gmail.com>
2016-01-30 19:02:27 -0600

    Add Monster Behavior

---
+++ Assets/Scripts/MonsterBehavior.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using UnityStandardAssets._2D;
+
+[RequireComponent(typeof (PlatformerCharacter2D))]
+public class MonsterBehavior : MonoBehaviour 
+{
+	private PlatformerCharacter2D m_Character;
+	private bool m_Jump;
+
+	private bool _stunned = false;
+
+	Transform _player = null;
+
+	float _currentDirection = -1;
+
+	public void Kill()
+	{
+		// kill Monster
+	}
+
+	public void Stun()
+	{
+
+	}
+
+	private void Awake()
+	{
+		m_Character = GetComponent<PlatformerCharacter2D>();
+	}
+
+	private void Start()
+	{
+		_player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+	}
+
+	private void Update()
+	{
+	}
+
+	private void FixedUpdate()
+	{
+	    // Read the inputs.
+	    // Pass all parameters to the character control script.
+	    m_Character.Move(_currentDirection, false, false);
+	}
+
+	void OnTriggerEnter2D( Collider2D other )
+	{
+		if ( other.CompareTag("MonsterBoundary"))
+		{
+			_currentDirection = -_currentDirection;
+		}
+	}
+}
