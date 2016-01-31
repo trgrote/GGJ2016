95e01e75977bada63e5f3556ef5588497a1fbc31
Taylor Grote <grotese@gmail.com>
2016-01-30 18:01:51 -0600

    Add Rain Quad and Scrolling Texture

---
+++ Assets/Scripts/ScrollingTexture.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingTexture : MonoBehaviour 
+{
+	[SerializeField] float speed = 1.0f;
+	
+	private Renderer render;
+
+	void Start()
+	{
+		render = GetComponent<Renderer>();
+
+		render.sortingLayerName = "Default";
+		render.sortingOrder = 1;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector2 new_offset = render.material.mainTextureOffset;
+		new_offset.y += Time.deltaTime * speed;
+		render.sharedMaterial.mainTextureOffset = new_offset;
+	}
+}
